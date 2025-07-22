28ec2885421b42d64a6e1194dfe053ebfde41e29
dipyourtrip <dipyourtrip@gmail.com>
2025-07-22 17:28:11 -0400

    Create report_payments.sql
    report with conciliated payments

---
+++ report_payments.sql
@@ -0,0 +1,60 @@
+WITH conciliated AS (
+  SELECT participant_email
+  FROM nat.wetravel_participants wp
+  INNER JOIN nat.travelers2 t 
+    ON wp.trip_id = t.trip_id AND wp.participant_email = t.email
+  -- WHERE wp.trip_id = 'Columbia_Chile_Club'
+  GROUP BY participant_email  -- required to maintain uniqueness per user input
+),
+reembolsos AS (
+  SELECT buyer_email, trip_id, SUM(amount) AS total_amount, COUNT(*) AS n_pays
+  FROM nat.wetravel_payments 
+  GROUP BY buyer_email, trip_id
+),
+participants_data AS (
+  SELECT 
+    wp.participant_email,
+    wp.participant_first_name,
+    wp.participant_last_name,
+    wp.trip_id,
+    r.total_amount,
+    wp.n_participants,
+    SAFE_DIVIDE(r.total_amount, wp.n_participants) AS amount_per_participant,
+    CASE WHEN c.participant_email IS NULL THEN 'not_conciliated' ELSE 'conciliated' END AS status
+  FROM nat.wetravel_participants wp
+  LEFT JOIN conciliated c 
+    ON c.participant_email = wp.participant_email
+  LEFT JOIN reembolsos r 
+    ON r.trip_id = wp.trip_id AND r.buyer_email = wp.buyer_email
+  -- WHERE wp.trip_id = 'Columbia_Chile_Club'
+    AND r.total_amount > 0
+),
+pays AS (
+  SELECT 
+    cp.typeform_email AS participant_email,
+    pd.participant_first_name,
+    pd.participant_last_name,
+    pd.trip_id,
+    pd.total_amount,
+    pd.n_participants,
+    pd.amount_per_participant
+  FROM participants_data pd
+  LEFT JOIN ops.conciliated_payments cp 
+    ON cp.participant_email = pd.participant_email
+  WHERE pd.status = 'not_conciliated' AND cp.state != 0
+  UNION ALL
+  SELECT 
+    pd.participant_email,
+    pd.participant_first_name,
+    pd.participant_last_name,
+    pd.trip_id,
+    pd.total_amount,
+    pd.n_participants,
+    pd.amount_per_participant
+  FROM participants_data pd
+  WHERE pd.status = 'conciliated'
+)
+SELECT r.*, p.total_amount, p.n_participants, p.amount_per_participant
+FROM pays p
+LEFT JOIN ops.report r 
+  ON r.trip_id = p.trip_id AND r.email = p.participant_email
